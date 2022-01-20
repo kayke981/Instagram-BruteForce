cff51161a7b44396e783d3d28962cd365cd0e028
kayake <77542672+kayke981@users.noreply.github.com>
2022-01-20 19:42:29 +0000

    ...

--- instagram.py
+++ instagram.py
@@ -2,15 +2,20 @@
 This tool can run in Python 3.9 or 3.10
 If have errors with passwords utf-8 or proxies please contact in github
 """
-try:
-	from lib.menu import menu
-	from lib.src.debug.debug import Debug
-except ImportError:
-	print("Not supported for python 2 please use python 3")
+
 import sys
+import os
 
 if sys.version_info[0] != 3:
 	print("[-] Not supported for python 2 please use python 3")
+	exit()
+
+try:
+	from lib.menu import menu
+except ModuleNotFoundError:
+	os.system('pip install -r requirements.txt')
+from lib.menu import menu
+from lib.src.debug.debug import Debug
 
 if __name__ == "__main__":
 	try:
