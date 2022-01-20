abd4391182fa297f549e74db473abf2499f56042
kayake <77542672+kayke981@users.noreply.github.com>
2022-01-20 19:17:26 +0000

    sorry to the person I think blocks the account, I thought it was mine

--- instagram.py
+++ instagram.py
@@ -1,13 +1,15 @@
-
 """ 
 This tool can run in Python 3.9 or 3.10
 If have errors with passwords utf-8 or proxies please contact in github
 """
 
 from lib.menu import menu
+from lib.src.debug.debug import Debug
+
+
 
 if __name__ == "__main__":
 	try:
 		menu()
 	except KeyboardInterrupt:
-		pass
+		Debug('[-] User interrupted')
