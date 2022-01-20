1ec5b11cae26d5f0d665ff17d13856556812cde9
kayake <77542672+kayke981@users.noreply.github.com>
2022-01-20 19:29:01 +0000

    ...

--- instagram.py
+++ instagram.py
@@ -2,9 +2,11 @@
 This tool can run in Python 3.9 or 3.10
 If have errors with passwords utf-8 or proxies please contact in github
 """
-
-from lib.menu import menu
-from lib.src.debug.debug import Debug
+try:
+	from lib.menu import menu
+	from lib.src.debug.debug import Debug
+except:
+	Debug("[-] Not supported for python 2 please use python 3", verbose=False)
 import sys
 
 if sys.version_info[0] != 3:
